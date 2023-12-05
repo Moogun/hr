0332c481ebfa3004f29c87f85d00cfe3071dd550
moogun <moogunjung@gmail.com>
2023-12-05 23:02:29 +0900

    working on box1

--- sections/box1.py
+++ sections/box1.py
@@ -21,9 +21,6 @@ class Box1(QFrame):
         label = QLabel('BOX-1 Future')
         vbox1.addWidget(label)
 
-        self.table = QTableWidget(1, 1)
-        vbox1.addWidget(self.table)
-
         vbox.addLayout(vbox1)
 
         # self.radio_btn1 = QRadioButton("P")
@@ -49,38 +46,41 @@ class Box1(QFrame):
         # # vbox2.addWidget(self.radio_btn4)
         #
         # # vbox.addLayout(vbox2)
-        # sh = self.p_instance.shcode
-        # self.label = QLabel('SH: '+ str(sh))
-        # self.line_edit = QLineEdit()
-        # self.line_edit .setFixedWidth(50)
-        # self.line_edit.textChanged.connect(self.on_text_changed)
-        #
+        sh = self.p_instance.shcode
+        self.label = QLabel('focode: '+ str(sh))
+        self.line_edit = QLineEdit()
+        self.line_edit .setFixedWidth(50)
+        self.line_edit.textChanged.connect(self.on_text_changed)
+
         # # button = QPushButton('Update')
         # # button.clicked.connect(self.on_button_clicked)
         # # button.setFixedWidth(50)
         #
-        # vbox2 = QVBoxLayout()
-        # vbox2.addWidget(self.label)
-        # vbox2.addWidget(self.line_edit)
+        vbox2 = QHBoxLayout()
+        vbox2.addWidget(self.label)
+        vbox2.addWidget(self.line_edit)
         # # vbox2.addWidget(button)
-        # vbox.addLayout(vbox2)
+        vbox.addLayout(vbox2)
+
+        self.table = QTableWidget(1, 1)
+        vbox.addWidget(self.table)
 
     def update_tr_future(self):
         print('update tr future')
 
-    # def on_radio_button_toggled(self):
-    #     if self.radio_btn1.isChecked():
-    #         self.p_instance.set_market('p')
-    #     elif self.radio_btn2.isChecked():
-    #         self.p_instance.set_market('q')
-    #
-    # def on_text_changed(self, text):
-    #     self.label.setText('SH-'+ text)
-    #     if len(text) == 6:
-    #         self.p_instance.set_shcode(text)
-    #         print('shcode updated', self.p_instance.shcode)
-    #     else:
-    #         print('not 6 digits')
-    #
+    def on_radio_button_toggled(self):
+        if self.radio_btn1.isChecked():
+            self.p_instance.set_market('p')
+        elif self.radio_btn2.isChecked():
+            self.p_instance.set_market('q')
+
+    def on_text_changed(self, text):
+        self.label.setText('SH-'+ text)
+        if len(text) == 6:
+            self.p_instance.set_shcode(text)
+            print('shcode updated', self.p_instance.shcode)
+        else:
+            print('not 6 digits')
+
     # def refresh(self):
     #     print('q')
