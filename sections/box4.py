e90309826fe423263efb65ab97d8b60589c68c52
moogun <moogunjung@gmail.com>
2023-12-07 23:49:50 +0900

    updated box4

--- sections/box4.py
+++ sections/box4.py
@@ -4,7 +4,7 @@ import numpy as np
 from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QLabel, QFrame, QPushButton, QTableWidget, QTableWidgetItem, QRadioButton
 import plotly.graph_objects as go
 from PyQt5.QtWebEngineWidgets import QWebEngineView
-
+from PyQt5.QtGui import QColor
 from q_params import Q_Params
 from color_book import color_dict
 
@@ -48,10 +48,7 @@ class Box4(QFrame):
     #     print('get_tr_half_min ', self.dfs)
     #
     #     self.table.clear()
-    #     print('self. dfs', self.dfs)
-    #     # csp_path = 'half_min.csv'
-    #     # self.dfs.to_csv(csp_path, index=False)
-    #
+
     #     self.dfs = self.dfs.drop(columns=['sign', 'open', 'high', 'low',
     #                                       'totofferrem', 'totbidrem', 'mdvolumetm', 'msvolumetm',
     #                                       'volume', 'mdchecnttm', 'mschecnttm', 'mdvolume', 'msvolume'
@@ -102,26 +99,40 @@ class Box4(QFrame):
     #         item.setBackground(color)
 
     def update_tr_market(self):
+        # self.dfs will be list
+        # items are dataframe and element of the items will be tuples
         self.dfs = self.model.get_tr_market()
 
+        print('shape ', type(self.dfs[0].shape))
+        print('shape ', self.dfs[0].shape)
+
         self.stock_p = self.dfs[0]
+        print('self.stock p index ', self.stock_p.index)
+        self.stock_p.index = ['피']
+        print('self.stock p', self.stock_p)
+
         self.stock_d = self.dfs[1]
+        self.stock_d.index = ['닥']
+
         self.option_p = self.dfs[2]
+        self.option_p.index = ['선물']
+
         self.option_d = self.dfs[3]
+        self.option_d.index = ['콜옵']
+
         self.future_p = self.dfs[4]
-        self.future_d = self.dfs[5]
+        self.future_p.index = ['풋옵']
 
-        self.table.clear()
+        self.newD = pd.concat([
+            self.stock_p, self.stock_d,
+            self.option_p, self.option_d,
+            self.future_p,
+                               ])
 
-        row_labels = [ 'SP_',
-                    'sd',
-                    'OP__',
-                    'od',
-                    'FP__',
-                    'fd',  ]
-
+        #
         dropping=['ms_08', 'md_08', 'rate_08',
                             'ms_17', 'md_17', 'rate_17',
+                            'ms_18', 'md_18', 'rate_18',
                             'ms_01', 'md_01', 'rate_01',
                             'ms_08', 'md_08', 'rate_08',
                             'ms_03', 'md_03', 'rate_03',
@@ -131,77 +142,117 @@ class Box4(QFrame):
                             'ms_06', 'md_06', 'rate_06',
                             'ms_11', 'md_11', 'rate_11',
                             'ms_07', 'md_07', 'rate_07',
-                            'ms_00', 'md_00', 'rate_00',  ]
+                            'ms_00', 'md_00', 'rate_00',
+                            'tjjcode_08',
+                            'jjcode_17',
+                            'jjcode_18',
+                            'jjcode_01',
+                            'jjcode_03',
+                            'jjcode_04',
+                            'jjcode_02',
+                            'jjcode_05',
+                            'jjcode_06',
+                            'jjcode_07',
+                            'jjcode_11',
+                            'jjcode_00',
+                            'svolume_11',
+                  ]
 
-        # self.dfs = self.dfs.rename(columns={'stimeqrt': '거래량전일동시간',
-        #                                     })
+        self.newD = self.newD.drop(columns=dropping)
+        self.newD = self.newD.rename(columns={
+            'svolume_08': '개',
+            'svolume_17': '외',
+            'svolume_18': '기계',
+            'svolume_01': '금투',
+            'svolume_04': '은',
+            'svolume_02': '보',
+            'svolume_03': '투신',
+            'svolume_05': '종금',
+            'svolume_06': '기금',
+            'svolume_07': '기타',
+            'svolume_00': '사모',
+                                            })
 
-        self.stock_p = self.stock_p.drop(columns=dropping)
-        self.stock_d = self.stock_d.drop(columns=dropping)
-        self.option_p = self.option_p.drop(columns=dropping)
-        self.option_d = self.option_d.drop(columns=dropping)
-        self.future_p = self.future_p.drop(columns=dropping)
-        self.future_d = self.future_d.drop(columns=dropping)
+        self.table.clear()
+        # pivot table by swapping row and col
+        num_rows, num_cols = self.newD.shape
 
-        num_rows, num_cols = self.stock_p.shape
+        self.table.setRowCount(num_cols)
+        self.table.setColumnCount(num_rows)
 
-        self.table.setRowCount(6)
-        self.table.setColumnCount(num_cols)
+        # # # Set the column headers to match the DataFrame column indices
+        # pivot table by swapping row and col
+        self.table.setHorizontalHeaderLabels(list(self.newD.index))
+        self.table.setVerticalHeaderLabels(list(self.newD.columns))
 
-        # # Set the column headers to match the DataFrame column indices
-        self.table.setHorizontalHeaderLabels(list(self.stock_p.columns))
-        self.table.setVerticalHeaderLabels(list(row_labels))
-
-        for row in range(len(row_labels)):
+        for row in range(num_rows):
             for col in range(num_cols):
-                print('row - ', row, 'col', col)
-                # item = QTableWidgetItem(str(self.dfs.iloc[row][col]))
-                # self.table.setItem(col, row, item)
+                item = QTableWidgetItem((self.newD.iloc[row][col]))
+
+                # if pd.to_numeric(self.newD.iloc[row, col]) > 0:
+                #     item.setBackground(QColor(0, 255, 0))  # green background for positive numbers
+
+                # pivot table by swapping row and col
+                self.table.setItem(col,row, item)
 
         self.table.resizeColumnsToContents()
         self.table.resizeRowsToContents()
 
+    def color_rows(self, num_rows, num_cols):
+        for row in range(num_rows):
+            cell_value = pd.to_numeric(self.newD.loc[row, 'diff'])
+            # if 10 <= cell_value < 20:
+            #     self.color_row(row, color_dict['gray1']) # whitesmoke
+            # elif 20 <= cell_value < 1000:
+            #     self.color_row(row, color_dict['gray3']) # lightGray
+            if 10 <= cell_value:
+                self.color_row(row, color_dict['gray3']) # lightGray
 
-    def update_chart(self, dfs):
-        x = dfs['chetime']
-        y = dfs['revolume']
-        bubble_text = dfs['cvolume']
+    def color_row(self, row, color):
+        row_items = [self.table.item(row, col) for col in range(self.table.columnCount())]
+        for item in row_items:
+            item.setBackground(color)
 
-        # x = dfs['hname']
-        #
-        # y = dfs['svalue']
-        y_diff = dfs['diff']
-
-        x = pd.to_numeric(x)
-        y = pd.to_numeric(y)
-        y_diff = pd.to_numeric(y_diff)
-        y_diff = np.int64(y_diff)
-
-        # def get_text_color(y_value):
-        #     return 'pink' if y_value > 0 else 'cornflowerblue'
-
-        fig = go.Figure(
-            data=[
-                go.Bar(x=x, y=y,  name='vol', marker=dict(color='grey')),
-                go.Scatter(x=x, y=y_diff,  yaxis='y2', mode = 'lines+markers', marker=dict(color='red')),
-                # go.Bar(x=x, y=y_diff, name='diff', yaxis='y2', marker=dict(color=[get_text_color(i) for i in y_diff])),
-                #
-            ],
-
-            layout={
-                'margin': {'l': 10, 'r': 10, 't': 30, 'b': 10},  # Minimized margins
-                'xaxis': {'showspikes': False, },
-                'yaxis': {'showspikes': False, },
-                'yaxis2': {'showspikes': False, 'overlaying': 'y', 'side': 'right', },
-            }
-        )
-
-        # Convert the Plotly chart to HTML
-        raw_html = '<html><head><meta charset="utf-8" /></head><body style="margin:0; padding:0;>'
-        content = fig.to_html(full_html=False, include_plotlyjs='cdn')
-        raw_html += content.replace("window.PlotlyConfig = {MathJaxConfig: 'local'};", "")
-        raw_html += '</body></html>'
-
-        # Set up the QWebEngineView
-        self.view.setHtml(raw_html)
-        self.hbox3.addWidget(self.view)
+    # def update_chart(self, dfs):
+    #     x = dfs['chetime']
+    #     y = dfs['revolume']
+    #     bubble_text = dfs['cvolume']
+    #
+    #     # x = dfs['hname']
+    #     #
+    #     # y = dfs['svalue']
+    #     y_diff = dfs['diff']
+    #
+    #     x = pd.to_numeric(x)
+    #     y = pd.to_numeric(y)
+    #     y_diff = pd.to_numeric(y_diff)
+    #     y_diff = np.int64(y_diff)
+    #
+    #     # def get_text_color(y_value):
+    #     #     return 'pink' if y_value > 0 else 'cornflowerblue'
+    #
+    #     fig = go.Figure(
+    #         data=[
+    #             go.Bar(x=x, y=y,  name='vol', marker=dict(color='grey')),
+    #             go.Scatter(x=x, y=y_diff,  yaxis='y2', mode = 'lines+markers', marker=dict(color='red')),
+    #             # go.Bar(x=x, y=y_diff, name='diff', yaxis='y2', marker=dict(color=[get_text_color(i) for i in y_diff])),
+    #             #
+    #         ],
+    #
+    #         layout={
+    #             'margin': {'l': 10, 'r': 10, 't': 30, 'b': 10},  # Minimized margins
+    #             'xaxis': {'showspikes': False, },
+    #             'yaxis': {'showspikes': False, },
+    #             'yaxis2': {'showspikes': False, 'overlaying': 'y', 'side': 'right', },
+    #         }
+    #     )
+    #
+    #     # Convert the Plotly chart to HTML
+    #     raw_html = '<html><head><meta charset="utf-8" /></head><body style="margin:0; padding:0;>'
+    #     content = fig.to_html(full_html=False, include_plotlyjs='cdn')
+    #     raw_html += content.replace("window.PlotlyConfig = {MathJaxConfig: 'local'};", "")
+    #     raw_html += '</body></html>'
+    #
+    #     # Set up the QWebEngineView
+    #     self.view.setHtml(raw_html)
+    #     self.hbox3.addWidget(self.view)
